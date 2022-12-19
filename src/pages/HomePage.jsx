27997eab5fd5c7b4b096e571e7a49b6f13bf6bc3
akhmedF98 <ahmedovfaridun98@gmail.com>
2022-12-19 12:55:48 +0600

    done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import ProductList from '../components/ProductList/ProductList'
+import ProductCard from '../components/ProductCard/ProductCard'
+import ProductDetails from '../components/ProductDetails/ProductDetails'
+import Footer from '../components/Footer/Footer'
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return(
+    <>
+      <ProductList />
+      <ProductCard />
+      <ProductDetails />
+      <Footer />
+    </>
+  )
 };
 
 export default HomePage;
