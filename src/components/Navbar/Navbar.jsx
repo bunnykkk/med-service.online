b4a18d4daa6c6317c95b288d23d7221b47144d93
akhmedF98 <ahmedovfaridun98@gmail.com>
2022-12-25 13:15:32 +0600

    done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,63 +1,62 @@
-import React from "react";
-import "../../styles/Navbar.css";
-import PersonIcon from "@mui/icons-material/Person";
-import logo from "../../img/logo.svg";
-import { useNavigate } from "react-router-dom";
+import React from 'react'
+import '../../styles/Navbar.css'
+import PersonIcon from '@mui/icons-material/Person'
+import logo from '../../img/logo.svg'
+import {useNavigate} from 'react-router-dom'
 
 const Navbar = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   return (
-    <div style={{ backgroundColor: "transparent" }}>
-      <div className="navbar">
+      <div className="navbar active">
         <div className="text">
-          <p className="logo">
+          <div className="logo">
             <img
               src={logo}
-              style={{ cursor: "pointer", height: "200px", width: "250px" }}
+              style={{cursor: 'pointer', height: '200px', width: '250px', marginTop: 60}}
               alt=""
-              onClick={() => navigate("/")}
+              onClick={() => navigate('/')}
             />
-          </p>
-          <p
-            className="p-1"
-            style={{ cursor: "pointer" }}
-            onClick={() => navigate("/direction")}>
-            Направление
-          </p>
-          <p
-            className="p-1"
-            style={{ cursor: "pointer" }}
-            onClick={() => navigate("/doctors")}>
-            Врачи
-          </p>
-          <p
-            className="p-1"
-            style={{ cursor: "pointer" }}
-            onClick={() => navigate("/contact")}>
-            Контакты
-          </p>
+          </div>
+            <p
+              className="p-1"
+              onClick={() => navigate('/')}>
+              Главная
+            </p>
+            <p
+              className="p-1"
+              onClick={() => navigate('/direction')}>
+              Направление
+            </p>
+            <p
+              className="p-1"
+              onClick={() => navigate('/doctors')}>
+              Врачи
+            </p>
+            <p
+              className="p-1"
+              onClick={() => navigate('/contact')}>
+              Контакты
+            </p>
           <div className="third">
             <p className="inp-p">
-              <input type="text" placeholder="Поиск" className="inp" />
+              <input type="text" placeholder="Поиск" className="inp"/>
             </p>
-            <div style={{ marginTop: "-20px" }}>
-              <PersonIcon className="icons" />
+            <div style={{marginTop: '-20px'}}>
+              <PersonIcon className="icons"/>
               <p
                 className="p-4"
-                style={{ cursor: "pointer" }}
-                onClick={() => navigate("/register")}>
+                onClick={() => navigate('/register')}>
                 Вход
               </p>
             </div>
-            <div style={{ marginTop: "-16px", marginRight: "60px" }}>
+            <div style={{marginTop: '-16px', marginRight: '60px'}}>
               <button className="btn"></button>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
